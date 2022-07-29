83b787aeac32ad4c96935568092aa8030b0c2e17
Evgeny <buriy-anarchist@inbox.ru>
2022-07-29 14:58:36 +0300

    Add task 52

---
+++ task_52/Program.cs
@@ -0,0 +1,39 @@
+﻿int[,] CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int k = new Random().Next(1, 10);
+            array[i, j] = k;
+        }
+    }
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+void AvgArray(int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        double avg = 0;
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            avg = avg + array[j, i];
+        }
+        Console.WriteLine(avg / array.GetLength(0));
+    }
+}
+
+int[,] array = new int[4, 4];
+array = CreateArray(array);
+AvgArray(array);
