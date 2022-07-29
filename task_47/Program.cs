b0050536c533ac7528133abe471c295350bd0a63
Evgeny <buriy-anarchist@inbox.ru>
2022-07-29 13:04:48 +0300

    Add task 47

---
+++ task_47/Program.cs
@@ -0,0 +1,11 @@
+﻿double[,] array = new double[3, 4];
+Random random = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = random.NextDouble() * 100;
+        Console.Write("{0,6:F2}", array[i, j]);
+    }
+    Console.WriteLine();
+}
